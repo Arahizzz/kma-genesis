42b9935edad26a44db6eb7af135043676ee708a2
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2021-12-01 15:06:50 +0200

    Delete models project

--- Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Messages;
-
-public class User
-{
-    public Guid Id { get; set; }
-    public string? Name { get; set; }
-    public string? Surname { get; set; }
-    public string? Email { get; set; }
-}
