fee8869fa8ea3ce43fc1abc2fa0a96f965ff8056
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2021-12-01 15:47:07 +0200

    Annotate email address field

--- ApiService/Models/User.cs
+++ ApiService/Models/User.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiService.Models;
 
 public class User
@@ -5,5 +7,6 @@ public class User
     public Guid Id { get; set; }
     public string? Name { get; set; }
     public string? Surname { get; set; }
+    [EmailAddress]
     public string? Email { get; set; }
 }
